6375add60695f51a6ca1f318124ad82727c8bb3d
Mosil <mosil.biz@gmail.com>
2023-03-14 16:07:32 +0800

    Listview basic commit

---
+++ android/app/src/main/kotlin/space/mosil/flutter/demo/listview/flutter_demo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package space.mosil.flutter.demo.listview.flutter_demo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DemoApp());
+}
+
+class DemoApp extends StatelessWidget {
+  const DemoApp({Key? key}) : super(key: key);
+  final String _title = "Flutter ListView Demo";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: AppColors.primary())),
+      home: MainApp(title: _title),
+    );
+  }
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  List<String> _getMockData() {
+    List<String> list = <String>[];
+    for (var i = 1; i <= 100; i++) {
+      list.add("My Item $i");
+    }
+    return list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        children: _buildMockListView(),
+      ),
+    );
+  }
+
+  List<ListTile> _buildMockListView() {
+    List<ListTile> list = <ListTile>[];
+    for (var i in _getMockData()) {
+      list.add(
+        ListTile(
+          title: Text(
+            i,
+            style: const TextStyle(fontSize: 20.0),
+          ),
+        ),
+      );
+    }
+    return list;
+  }
+}
+
+class AppColors {
+  static final Color _primaryColor = Color(0xff0a3042);
+
+  static MaterialColor primary() {
+    return _getMaterialColor(_primaryColor);
+  }
+
+  static MaterialColor _getMaterialColor(Color color) {
+    final int red = color.red;
+    final int green = color.green;
+    final int blue = color.blue;
+
+    final Map<int, Color> shades = {
+      50: Color.fromRGBO(red, green, blue, .1),
+      100: Color.fromRGBO(red, green, blue, .2),
+      200: Color.fromRGBO(red, green, blue, .3),
+      300: Color.fromRGBO(red, green, blue, .4),
+      400: Color.fromRGBO(red, green, blue, .5),
+      500: Color.fromRGBO(red, green, blue, .6),
+      600: Color.fromRGBO(red, green, blue, .7),
+      700: Color.fromRGBO(red, green, blue, .8),
+      800: Color.fromRGBO(red, green, blue, .9),
+      900: Color.fromRGBO(red, green, blue, 1),
+    };
+
+    return MaterialColor(color.value, shades);
+  }
+}
